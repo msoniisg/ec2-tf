a0779526009792820327fa5ccf47a92db05b14bf
msoni@infosenseglobal.com <msoni@infosenseglobal.com>
2024-06-26 20:27:44 +0530

    modified:   Terraform/output.tf 	modified:   inventory.ini

--- Terraform/output.tf
+++ Terraform/output.tf
@@ -2,6 +2,12 @@ output "server_public_ip" {
   value = aws_eip.web-eip.public_ip
 }
 
+resource "null_resource" "store_public_ip" {
+  provisioner "local-exec" {
+      command = "echo '${aws_eip.web-eip.public_ip}' > public_ip.txt"
+  }
+}
+
 output "server_privet_ip" {
   value = aws_instance.web-server-instance.private_ip
 }
